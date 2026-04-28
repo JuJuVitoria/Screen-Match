424137a5822174bc5bef78e573b315ffc7306c8d
JuJuVitoria <juliavitoriadaluz5@gmail.com>
2026-04-27 21:15:24 -0300

    Aula-1: Remodelando o Screen Match.

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Esse é o Screen Match");
-        System.out.println("Filme: Top Gun: Maverick");
+        Filme meuFilme = new Filme("O poderoso chefão", 1970, 180);
+        meuFilme.exibeFichaTecnica();
 
-        int anoDeLancamento = 2022;
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-        boolean incluidoNoPlano = true;
-        double notaDoFilme = 8.1;
-        double media = (9.8 + 6.3 + 8.0) /3;
-        System.out.println(media);
-        String sinopse;
-        sinopse = """
-                Filme Top Gun
-                Filme de aventura com galã dos anos 80
-                Muito bom!
-                Ano de lançamento
-                """ + anoDeLancamento;
-        System.out.println(sinopse);
-
-        int classificacao = (int) (media /2);
-        System.out.println(classificacao);
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.pegaMedia());
     }
 }
