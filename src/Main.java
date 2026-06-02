4e834835bd5aca5f4b34426616302d0a8108be0f
JujuVitoria <juliavitoridaluz5@gmail.com>
2026-06-02 15:12:45 -0300

    Curso-2 | Aula-2: cuidando do encapsulamento.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
+import br.com.alura.screenmatch.models.Filme;
+
 public class Main {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme("O poderoso chefão", 1970, 180);
-        meuFilme.exibeFichaTecnica();
+        Filme filme1 = new Filme("O poderoso chefão", 1970, 180);
+        filme1.exibeFichaTecnica();
 
-        meuFilme.avalia(8);
-        meuFilme.avalia(5);
-        meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
-        System.out.println(meuFilme.pegaMedia());
+        filme1.avalia(8);
+        filme1.avalia(5);
+        filme1.avalia(10);
+        System.out.println("Total de avaliações: " + filme1.getTotalDeAvaliacoes());
+        System.out.println(filme1.pegaMedia());
     }
 }
