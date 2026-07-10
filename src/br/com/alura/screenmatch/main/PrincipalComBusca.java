b8d86479133117647aea549ccd607543842f606c
JuJuVitoria <juliavitoriadaluz5@gmail.com>
2026-07-10 17:49:40 -0300

    Curso-4 | Aula-02: Utilizando uma biblioteca externa (Gson) para transformar json em objeto.

--- src/br/com/alura/screenmatch/main/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/main/PrincipalComBusca.java
@@ -1,5 +1,11 @@
 package br.com.alura.screenmatch.main;
 
+import br.com.alura.screenmatch.models.Titulo;
+import br.com.alura.screenmatch.models.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -25,6 +31,17 @@ public class PrincipalComBusca {
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
+        TituloOmdb tituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(tituloOmdb);
+        Titulo titulo = new Titulo(tituloOmdb);
+        System.out.println(titulo.getNome());
+        System.out.println(titulo.getAnoDeLancamento());
+        System.out.println(titulo.getDuracaoEmMinutos());
     }
 }
--- src/br/com/alura/screenmatch/models/Titulo.java
+++ src/br/com/alura/screenmatch/models/Titulo.java
@@ -1,7 +1,11 @@
 package br.com.alura.screenmatch.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -11,6 +15,12 @@ public class Titulo implements Comparable<Titulo> {
     public Titulo(String nome, int anoDeLancamento){
         this.nome = nome;
         this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Titulo(TituloOmdb tituloOmdb) {
+        this.nome = tituloOmdb.title();
+        this.anoDeLancamento = Integer.valueOf(tituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(tituloOmdb.runtime().substring(0, 3));
     }
 
     public String getNome() {
