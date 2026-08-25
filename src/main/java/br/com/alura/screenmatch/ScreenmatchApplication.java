54e165dfd8c65c1c970cc3df4687d5411c088e1e
JuJuVitoria <juliavitoriadaluz5@gmail.com>
2026-08-24 21:02:22 -0300

    feat: implementa menu interativo

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -5,6 +5,7 @@ import br.com.alura.screenmatch.model.SeasonInfo;
 import br.com.alura.screenmatch.model.SerieInfo;
 import br.com.alura.screenmatch.service.ConsumeAPI;
 import br.com.alura.screenmatch.service.DataConvert;
+import br.com.alura.screenmatch.view.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,25 +22,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ConsumeAPI consumeAPI = new ConsumeAPI();
-		DataConvert dataConvert = new DataConvert();
-
-		var json = consumeAPI.getData("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		SerieInfo serieInfo = dataConvert.getData(json, SerieInfo.class);
-		System.out.println(serieInfo);
-
-		List<SeasonInfo> seasons = new ArrayList<>();
-
-		for (int i = 1; i <= serieInfo.numberSeasons(); i++) {
-			json = consumeAPI.getData("https://www.omdbapi.com/?t=" + serieInfo.title() + "&season=" + i + "&apikey=6585022c");
-			SeasonInfo seasonInfo = dataConvert.getData(json, SeasonInfo.class);
-			seasons.add(seasonInfo);
-		}
-
-		seasons.forEach(System.out::println);
-
-		json = consumeAPI.getData("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
-		EpisodeInfo episodeInfo = dataConvert.getData(json, EpisodeInfo.class);
-		System.out.println(episodeInfo);
+		Main main = new Main();
+		main.showMenu();
 	}
 }
---
+++ src/main/java/br/com/alura/screenmatch/view/Main.java
@@ -0,0 +1,46 @@
+package br.com.alura.screenmatch.view;
+
+import br.com.alura.screenmatch.model.EpisodeInfo;
+import br.com.alura.screenmatch.model.SeasonInfo;
+import br.com.alura.screenmatch.model.SerieInfo;
+import br.com.alura.screenmatch.service.ConsumeAPI;
+import br.com.alura.screenmatch.service.DataConvert;
+
+import java.awt.datatransfer.Clipboard;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    private Scanner scanner = new Scanner(System.in);
+    private final String API_ADDRESS = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "6585022c";
+    private String url;
+
+    private ConsumeAPI consumeAPI = new ConsumeAPI();
+    private DataConvert dataConvert = new DataConvert();
+
+    public void showMenu() {
+        System.out.println("Digite o nome da série: ");
+        var serieName = scanner.nextLine();
+        serieName = serieName.replace(" ", "+").toLowerCase().strip();
+
+        //https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c
+        url = API_ADDRESS + serieName + "&apikey=" + API_KEY;
+        System.out.println(url);
+
+        var json = consumeAPI.getData(url);
+        SerieInfo serieInfo = dataConvert.getData(json, SerieInfo.class);
+        System.out.println(serieInfo);
+
+        List<SeasonInfo> seasons = new ArrayList<>();
+
+        for (int i = 1; i <= serieInfo.numberSeasons(); i++) {
+            json = consumeAPI.getData(API_ADDRESS + serieName + "&season=" + i + "&apikey=" + API_KEY);
+            SeasonInfo seasonInfo = dataConvert.getData(json, SeasonInfo.class);
+            seasons.add(seasonInfo);
+        }
+
+        seasons.forEach(System.out::println);
+    }
+}
